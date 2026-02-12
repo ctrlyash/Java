2b4fbf5ba4b94b61a59f08218a73e64d80256de9
Yash Dadhich <yashdadhich9934@gmail.com>
2026-02-12 23:43:52 +0530

    First code in java

---
+++ Basics/01_main/Main.java
@@ -0,0 +1,43 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello Java!");
+        System.out.println("My name is Yash");
+    }
+}
+
+// O/P:-
+// Hello Java!
+// My name is Yash
+
+
+// Notes:-
+// public is access modifier
+
+// class is a keyword to define a class
+
+// Main is the name of the class
+
+// {} is a block
+
+// static is a keyword to define a static method
+
+// it is a method that can be called without creating an object
+
+// main is the name of the method
+
+// String[] args is the parameter of the method
+
+// [] signifies an array  in which the parameters are stored in the form of strings and args is the name of the array
+
+// System is a class
+
+// out is a static field of the class System (static field is a field that is shared by all the objects of the class)
+
+// println is a method of the class System that is used to print the output
+// ln is used for new line
+
+// Hello Java! is the argument of the method println
+
+// Name of a class in java is same as the name of the file and it should start in uppercase.
+
+// The name of the method must be main because it is the entry point of the program and main is a reserved word in java for the entry point of a program
