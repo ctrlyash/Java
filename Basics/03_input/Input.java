5b342a78c9a793e3bc8c22e2ca695d64a7982943
Yash Dadhich <yashdadhich9934@gmail.com>
2026-02-13 23:40:47 +0530

    (edited) using Scanner class in java

---
+++ Basics/03_input/Input.java
@@ -0,0 +1,45 @@
+import java.util.Scanner;
+public class Input {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int num = input.nextInt();
+        System.out.println("You entered: " + num);
+
+        System.out.print("Enter a name: ");
+        String name = input.next();
+        System.out.println("You entered: " + name);
+
+         System.out.print("Enter marks: ");
+        float marks = input.nextFloat();
+        System.out.println("You entered: " + marks);
+    }
+}
+
+// O/P:-
+// Enter a number: 5
+// You entered: 5
+
+// Enter a name: Yash
+// You entered: Yash
+
+// Enter marks: 90.456799
+// You entered: 90.4568
+// This is a floating point error (Values are not accurate in float)
+
+
+// Notes:-
+// Scanner is a class that is used to take input from the user
+
+// input is an object of the class Scanner
+
+// System.in is a static field of the class System that is used to take input from the user
+// Difference b/w System.in and System.out is that System.in is used to take input from the user whereas System.out is used to print the output
+
+// nextInt() is a method of the class Scanner that is used to take input as an integer from the user
+// next() is a method of the class Scanner that is used to take input from the user as string
+
+// nextFloat() is a method of the class Scanner that is used to take input as a float from the user
+
+
+
