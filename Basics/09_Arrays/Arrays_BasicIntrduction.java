8769459247e878d07eb97136b2c003f63a89a236
Yash Dadhich <yashdadhich9934@gmail.com>
2026-03-04 22:43:06 +0530

    Basic introduction about arrays in java

---
+++ Basics/09_Arrays/Arrays_BasicIntrduction.java
@@ -0,0 +1,38 @@
+public class Arrays_BasicIntrduction { 
+    public static void main(String[] args) {
+
+        // Why do we need arrays?
+
+        // Lets suppose we need to store and print roll numbers of 5 students 
+
+        int roll_1 = 1;
+        int roll_2 = 2;
+        int roll_3 = 3;
+        int roll_4 = 4;
+        int roll_5 = 5;
+
+        System.out.println("Roll number of student 1: " + roll_1);
+        System.out.println("Roll number of student 2: " + roll_2);
+        System.out.println("Roll number of student 3: " + roll_3);
+        System.out.println("Roll number of student 4: " + roll_4);
+        System.out.println("Roll number of student 5: " + roll_5);
+
+        // Now imagine we have to store and print roll numbers of 100 students, it would be very tedious and inefficient to declare 100 variables and print them one by one. This is where arrays come in handy.
+
+        // Array is a data structure that can store multiple values of the same type in a single variable.
+        // It is a collection of elements, each identified by an index or key.
+
+        /*
+        Syntax:
+
+        dataType[] arrayName = new dataType[size];
+
+        This datatype represents the type of elements that the array will hold, which can be any primitive data type (like int, double, char) or even objects.
+
+        The arrayName is the name of the array.
+
+        The new keyword is used to create a new array and size is the number of elements that the array can hold.
+        */
+
+    }
+}
